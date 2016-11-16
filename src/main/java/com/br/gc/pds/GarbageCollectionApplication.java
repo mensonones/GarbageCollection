74b108ff86e4af1e173465aa1817d37b06b09401
Emerson Vieira <mensones.1@gmail.com>
2016-11-16 10:35:20 -0300

    projeto inicial

---
+++ src/main/java/com/br/gc/pds/GarbageCollectionApplication.java
@@ -0,0 +1,12 @@
+package com.br.gc.pds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbageCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarbageCollectionApplication.class, args);
+	}
+}
