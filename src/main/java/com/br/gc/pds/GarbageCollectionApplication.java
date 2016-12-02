0d2720552f42b6075e76ce781be53c05bf1d1971
Emerson Vieira <mensones.1@gmail.com>
2016-12-02 13:00:34 -0200

    Adaptação do layout pro thymeleaf

--- src/main/java/com/br/gc/pds/GarbageCollectionApplication.java
+++ src/main/java/com/br/gc/pds/GarbageCollectionApplication.java
@@ -3,9 +3,10 @@ package com.br.gc.pds;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class GarbageCollectionApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GarbageCollectionApplication.class, args);
 	}
---
+++ src/main/java/com/br/gc/pds/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.br.gc.pds.config;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class WebConfig extends WebMvcConfigurerAdapter {
+	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        
+}
+}
